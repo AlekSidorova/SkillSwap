cf59349621dac0cd51b0cddf071f9470aaba1ad6
MrKarlLogan <mrelectricguitar7@gmail.com>
2025-11-29 13:07:52 +0300

    feat: добавлен универсальный компонент для страницы ошибок. Добавлены алиасы на папки первого порядка. Исправлены импорты. Миксины и переменные являются теперь глобальными. Исправлены баги в компоненте input.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@styles/variables" as *;
+          @use "@styles/mixins" as *;
+        `,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@app": path.resolve(__dirname, "./src/app"),
+      "@images": path.resolve(__dirname, "./src/images"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
+      "@store": path.resolve(__dirname, "./src/store"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      "@widgets": path.resolve(__dirname, "./src/widgets"),
+    },
+  },
 });
