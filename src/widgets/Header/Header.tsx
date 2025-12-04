807503b75837030ffd9ae39ac8fb13a1372f7f35
Dmitrii Timonov <42276523+creator-of-masterpieces@users.noreply.github.com>
2025-12-05 00:43:46 +0300

    fix(Header): Доработал сетку шапки;
    Стилизовал сетку шапки при авторизованном пользователе по pp;

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -101,7 +101,7 @@ export const Header = () => {
           <Logo />
         </Link>
 
-        <ul className={styles.navigationLinkContainer}>
+        <ul className={styles.navigationList}>
           <li>
             <Link to="/" className={styles.navigationLink}>
               О проекте
@@ -139,7 +139,6 @@ export const Header = () => {
           </li>
         </ul>
       </nav>
-
       <div className={styles.searchWrapper} ref={searchRef}>
         <form className={styles.search} onSubmit={handleSearchSubmit}>
           <Input
@@ -165,10 +164,12 @@ export const Header = () => {
         )}
       </div>
 
+      {/* Компоненты доступные при авторизованном пользователе */}
       {isAuth ? (
-        <>
-          <div className={styles.decorateButtonsWrapper}>
+        <div className={styles.ButtonsProfileWrapper}>
+          <div className={styles.buttons}>
             <DecoratedButton variant={"moon"} onClick={() => toggle()} />
+
             <div data-trigger-dropdown="notifications">
               <DecoratedButton
                 variant="bell"
@@ -204,39 +205,38 @@ export const Header = () => {
               src="https://i.pravatar.cc/150?img=17"
               alt="Аватар пользователя"
             />
+            {isMenuOpen && (
+              <DropDown
+                top="40px"
+                left="-260px"
+                triggerGroupe="profile"
+                onClose={() => {
+                  setIsMenuOpen(false);
+                }}
+              >
+                <ul className={styles.profileMenuList}>
+                  <li className={styles.profileMenuItem}>
+                    <Link to={"/profile"}>Личный кабинет</Link>
+                  </li>
+                  <li
+                    className={clsx(
+                      styles.profileMenuItem,
+                      styles.profileMenuItemExit,
+                    )}
+                    onClick={() => console.log("Вы вышли из аккаунта")}
+                  >
+                    Выйти из аккаунта
+                  </li>
+                </ul>
+              </DropDown>
+            )}
           </div>
-          {isMenuOpen && (
-            <DropDown
-              top="20px"
-              left="1105px"
-              triggerGroupe="profile"
-              onClose={() => {
-                setIsMenuOpen(false);
-              }}
-            >
-              <ul className={styles.profileMenuList}>
-                <li className={styles.profileMenuItem}>
-                  <Link to={"/profile"}>Личный кабинет</Link>
-                </li>
-                <li
-                  className={clsx(
-                    styles.profileMenuItem,
-                    styles.profileMenuItemExit,
-                  )}
-                  onClick={() => console.log("Вы вышли из аккаунта")}
-                >
-                  Выйти из аккаунта
-                </li>
-              </ul>
-            </DropDown>
-          )}
-        </>
+        </div>
       ) : (
         <>
-          <div className={styles.decorateButtonsWrapper}>
+          <div className={styles.decorateButtons}>
             <DecoratedButton variant={"moon"} onClick={() => toggle()} />
           </div>
-
           <div className={styles.authButtons}>
             <Link to="/login" className={styles.navLink}>
               <Button children={"Войти"} variant="secondary"></Button>
@@ -247,59 +247,6 @@ export const Header = () => {
             </Link>
           </div>
         </>
-      )}
-      {/* // ToDo Заменить на меню профиля когда оно будет готово */}
-
-      {/* // ToDo Заменить на окно с уведомлениями когда оно будет готово */}
-      {/* {isNotificationsOpen && (
-        <DropDown
-          top="20px"
-          left="812px"
-          onClose={() => setIsNotificationsOpen(false)}
-        >
-          <NotificationPanel />
-        </DropDown>
-      )} */}
-      {/* // ToDo Заменить на окно с категориями когда оно будет готово */}
-      {isCategoriesMenuOpen && (
-        <div className={styles.categoriesMenu}>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
-            delectus dolorem doloribus eligendi harum illum, itaque laboriosam
-            maxime natus nostrum numquam perspiciatis, provident qui recusandae
-            repudiandae sed tempore, veniam voluptatem?
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
-            delectus dolorem doloribus eligendi harum illum, itaque laboriosam
-            maxime natus nostrum numquam perspiciatis, provident qui recusandae
-            repudiandae sed tempore, veniam voluptatem?
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
-            delectus dolorem doloribus eligendi harum illum, itaque laboriosam
-            maxime natus nostrum numquam perspiciatis, provident qui recusandae
-            repudiandae sed tempore, veniam voluptatem?
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
-            delectus dolorem doloribus eligendi harum illum, itaque laboriosam
-            maxime natus nostrum numquam perspiciatis, provident qui recusandae
-            repudiandae sed tempore, veniam voluptatem?
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
-            delectus dolorem doloribus eligendi harum illum, itaque laboriosam
-            maxime natus nostrum numquam perspiciatis, provident qui recusandae
-            repudiandae sed tempore, veniam voluptatem?
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
-            delectus dolorem doloribus eligendi harum illum, itaque laboriosam
-            maxime natus nostrum numquam perspiciatis, provident qui recusandae
-            repudiandae sed tempore, veniam voluptatem?
-          </p>
-        </div>
       )}
     </header>
   );
